fcde38cf09da5ca4d6695df832a73369c3ccbbe2
Shabad <shabadmk969@outlook.com>
2026-04-07 21:16:23 +0530

    cutome domain

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -46,6 +46,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "https://autohead-iqy3.vercel.app",
+    "https://adm.autoheadknr.com",
 ]
 
 CORS_ALLOW_CREDENTIALS = False
