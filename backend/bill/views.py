7e36c29d6453467c2b5134b10c99f92c81f4f787
Shabad <shabadmk969@outlook.com>
2026-04-15 13:36:20 +0530

    final production test

--- backend/bill/views.py
+++ backend/bill/views.py
@@ -10,7 +10,7 @@ from django.db.models import Q
 # Create your views here.
 
 class BillPagination(PageNumberPagination):
-    page_size = 20
+    page_size = 10
     page_size_query_param = 'page_size'
     max_page_size = 1000
     
