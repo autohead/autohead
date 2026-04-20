7c7d7c93446dbba9dcefc88cabbe5311a41c442d
Shabad <shabadmk969@outlook.com>
2026-04-20 22:22:55 +0530

    fixing delete error

--- backend/product/views.py
+++ backend/product/views.py
@@ -122,17 +122,17 @@ class ProductUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
         method = "DEACTIVATE" if instance.is_active else "REACTIVATE"
         return custom_response(data=None, method=method, data_name="Product")
 
-from rest_framework.response import Response
+
 class DeleteAllProductsView(generics.GenericAPIView):
     permission_classes = [IsAuthenticated]
 
-    # def delete(self, request, *args, **kwargs):
-    #     Products.objects.all().delete()
-    #     return custom_response(data=None, method="DELETE", data_name="Products")
-    
     def delete(self, request, *args, **kwargs):
-        Products.objects.all().delete()
-        return Response({"message": "Deleted"})
+        batch_size = 5
+        qs = Products.objects.all()
+        while qs.exists():
+            ids = list(qs.values_list("id", flat=True)[:batch_size])
+            Products.objects.filter(id__in=ids).delete()
+        return custom_response(data=None, method="DELETE", data_name="Products")
 
 # View for listing products and vendors for dropdowns.
 class DropdownDataList(generics.ListAPIView):
