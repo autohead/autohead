7eba7098502e576c6b14caf599094a96ac7a0b05
Shabad <shabadmk969@outlook.com>
2026-04-20 22:28:39 +0530

    fixing delete error

--- backend/product/views.py
+++ backend/product/views.py
@@ -127,13 +127,7 @@ class DeleteAllProductsView(generics.GenericAPIView):
     permission_classes = [IsAuthenticated]
 
     def delete(self, request, *args, **kwargs):
-        batch_size = 10
-        qs = Products.objects.all()
-        while qs.exists():
-            ids = list(qs.values_list("id", flat=True)[:batch_size])
-            if not ids:
-                break
-            Products.objects.filter(id__in=ids).delete()
+        Products.objects.all().delete()
         return custom_response(data=None, method="DELETE", data_name="Products")
 
 # View for listing products and vendors for dropdowns.
